2cdcfcc7ca5103c8396a9c5c4ff2c739b99b4317
Vebu Tharmanithy <vebuvebu115@gmail.com>
2026-02-08 22:03:20 +0000

    Initial React app setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Job Application Tracker</h1>
+
+      <h3>Add a Job</h3>
+
+      <input placeholder="Job Title" />
+      <br /><br />
+
+      <input placeholder="Company Name" />
+      <br /><br />
+
+      <button>Add Job</button>
+    </div>
+  );
+}
+
+export default App;
